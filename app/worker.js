26ee30c1df1a0946b2e47aa04cb1dd993d448584
Gabriel Leandro <gabrielleandrojunior@live.com>
2018-05-10 08:36:00 -0300

    Update

--- app/worker.js
+++ app/worker.js
@@ -1,5 +1,6 @@
 import { CronJob, CronTime } from 'cron';
 import moment from 'moment';
+import Decimal from 'decimal.js';
 
 const EVERY_MINUTE_CRON_EXPRESSION = '* * * *';
 
@@ -10,60 +11,70 @@ export default class BackgroundWorker {
     }
 
     run() {
-        this.app.providers.binance.ws.candles('ETHBTC', '1m', async kline => {
-            let result = await this.app.providers.db.collection('kline').insert({
-                symbol: kline.symbol,
-                eventTime: kline.eventTime,
-                open: parseFloat(kline.open),
-                close: parseFloat(kline.close),
-                high: parseFloat(kline.high),
-                low: parseFloat(kline.low)
+        setInterval(async () => {
+            return;
+            let prices = await this.app.providers.binance.prices()
+            let keys = Object.keys(prices);
+            keys.map(async (key) => {
+                let result = await this.app.providers.db.collection('prices').insert({
+                    symbol: key,
+                    eventTime: moment().valueOf(),
+                    price: parseFloat(prices[key])
+                });
             });
-        });
+            console.log(`Inserted more: ${keys.length}`);
+        }, 1000);
 
         async function consilidaVela(db) {
-            let start = moment().subtract(60, "minutes").valueOf();
+            let start = moment().subtract(1, "minutes").valueOf();
             let end = moment().valueOf();
 
-            let result = await db.collection('kline').aggregate(
-                [
-                    {
-                        $match: {
-                            eventTime: {
-                                $gte: start,
-                                $lt: end
-                            }
-                        }
-                    },
-                    {
-                        $group: {
-                            _id: '$symbol',
-                            itemsCount: { $sum: 1 },
-                            totalClose: { $sum: '$close' },
-                            avgClose: { $avg: '$close' },
-                            maxClose: { $max: '$close' },
-                            minClose: { $min: '$close' }
-                        }
+            let result = await db.collection('prices').find({
+                symbol: 'ETHBTC',
+                eventTime: {
+                    $gte: start,
+                    $lt: end
+                }
+            }).toArray();
+
+
+            let value = {
+                last: null,
+                up: 0,
+                down: 0
+            };
+
+            result.map((current, i) => {
+                if (value.last != null) {
+                    let r = (current.price - value.last.price);
+                    //console.log(r.toString());
+                    if (r > 0) {
+                        value.up += r;
+                    } else {
+                        value.down += -r;
                     }
-                ]).toArray();
-
-            result = result[0];
-            result.symbol = result._id;
-            delete result._id;
-            result.startTime = start;
-            result.endTime = end;
-            result.created_at = moment().valueOf();
+                }
+                value.last = current;
+            });
+            console.log(value.up);
+            console.log(value.down);
+            //value.up = value.up/result.lenght;
+            //value.down = value.down/result.lenght;
+            //console.log(Math.round);
+            /*let fr = Math.round(value.up) / Math.round(value.down);
+            let ifr = 100 - (100 / (1 + fr));
+            console.log(ifr);*/
             await db.collection('velas').insert(result);
-            console.log(result);
         };
-
+        consilidaVela(this.app.providers.db).then(() => { });
+        /*
         this.everyMinuteJob = new CronJob({
             cronTime: EVERY_MINUTE_CRON_EXPRESSION,
             onTick: () => consilidaVela(this.app.providers.db).then(() => { }),
             start: false,
             timeZone: 'America/Sao_Paulo'
         });
-        this.everyMinuteJob.start();
+        this.everyMinuteJob.start();*/
     }
 
 }
