c0b41cddb7f32f225e0a0ef61abc84cda697a22f
Gabriel <gabrielleandrojunior@live.com>
2018-07-18 19:21:50 -0300

    Update Log Timezone

--- app/services/logger.js
+++ app/services/logger.js
@@ -1,8 +1,8 @@
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 const OUT_DATE_FORMAT = 'DD/MM/Y HH:mm:ss';
 
 export default function log(msg) {
-    const date = moment().format(OUT_DATE_FORMAT);
+    const date = moment().tz('America/Sao_Paulo').format(OUT_DATE_FORMAT);
     console.log(`${date}: ${msg}`);
 };
